9b587d71486729328661165b9e3d9960dad2ee72
max <max@mbp-max.local>
2022-08-18 10:49:39 +0400

    chore: update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         background_color: `#dbdbdb`,
         theme_color: `#dbdbdb`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
